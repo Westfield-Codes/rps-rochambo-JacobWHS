2e14983abd5b3d541f65cdac548425ffafe01484
lin., frank <jacob127871@student-schoolsofwestfield.org>
2024-11-18 16:19:27 +0000

    first

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 const moves = ["r", "p", "s"];
+const movesFormatted = ["Rock", "Paper", "Scissors"];
+const players = ["You", "I"];
+var score = [0, 0];
 
 function main(){
+    let rounds = parseInt(setRounds());
+    let round = parseInt(0);
+    while (round <= rounds){ 
+        if (!round <= rounds){
+            alert("no");
+        }
+        round++;
+        alert("Round: " + round + " Rounds: " + rounds)
+        if (round == 1) alert("ROUND ONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        let winner = rpsRound();
+        score[winner]++;
+        alert(winner);
+    }
+    alert("It's " + score[0] + " to " + score[1] + "!");
+    if (score[0] > score[1]) alert("You win!");
+    else alert("You lose...")
+    let again = confirm("Could I interest you in playing again?");
+    if (again == true) main();
+}
+
+function setRounds(){
+    let r = parseInt(prompt("How many rounds of rock paper scissors would you like to play?"))
+    if (r % 2 == 0 || isNaN(r)){
+        alert("Your response must be an even number!");
+        return setRounds();
+    }
+    return r;
+}
+
+function rpsRound(){
     let uChoice = 0;
     let cChoice = 0;
     while (uChoice == cChoice){
@@ -8,10 +41,13 @@ function main(){
         cChoice = cpuTurn();
         if (uChoice == cChoice) alert("Erm, what the sigma? We both did " + uChoice + "??!?1/1//1/");
     }
-    findWinner(uChoice, cChoice);
-    alert("I chose " + cChoice + ", so " + winner + " won!")
-    let again = confirm("Wanna play 'gain?");
-    if (again == true) main();
+    let winner = findWinner(uChoice, cChoice);
+    let win = players.indexOf(winner);
+    // alert("uChoice: " + uChoice);
+    // alert("cChoice: " + cChoice);
+    // alert("Winner: " + winner);
+    // alert("Win: " + win);
+    return win;
 }
 
 function userTurn(){
@@ -24,8 +60,8 @@ function userTurn(){
 }
 
 function cpuTurn(){
-    let choice = Math.floor(Math.random()*2);
-    alert(moves[choice] + "!")
+    let choice = Math.floor(Math.random()*3);
+    alert(movesFormatted[choice] + "!")
     return moves[choice];
 }
 
@@ -37,10 +73,10 @@ function findWinner(u, c){
         ["s", "r", "I"], ["s", "p", "you"]];
     turns = u + c;
     for (let x = 0; x < wA.length; x++){
-        match = wA[x][0] + [x][1];
+        match = wA[x][0] + wA[x][1];
         if (match == turns){ 
             winner = wA[x][2];
             return winner;
-            }            
+        }            
     }
 }
